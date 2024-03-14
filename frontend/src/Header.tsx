2ab4e8d61612f5a3571d9bd8f1827ef5ca7601a0
apeal2real <alec.peal@gmail.com>
2024-03-14 10:57:55 -0600

    Updated header

--- frontend/src/Header.tsx
+++ frontend/src/Header.tsx
@@ -2,6 +2,10 @@ function Header(props: any) {
   return (
     <header className="row navbar">
       <h1>Bowler Database</h1>
+      <h3>
+        This webpage displays information about Bowler's on the Marlins and
+        Sharks bowling teams.
+      </h3>
     </header>
   );
 }
